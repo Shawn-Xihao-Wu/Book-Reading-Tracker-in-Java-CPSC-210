a1f7756adf4589a31986413e5828edf1d9e4683d
shawnxihaowu <shawnxhw@students.cs.ubc.ca>
2022-01-27 21:53:16 -0800

    Specifying data abstraction

---
+++ src/main/model/Book.java
@@ -0,0 +1,34 @@
+package model;
+
+
+// Represents a book that I am interested in reading that has
+// a title, total page #, # of pages I have read, progress in terms of percentage
+public class Book {
+
+    // REQUIRES:
+    public Book(String title, int pages) {
+        // stub
+    }
+
+    public void progressUpdate(int pages) {
+
+    }
+
+
+    public String getTitle() {
+        return null;
+    }
+
+    public int getPages() {
+        return -1;
+    }
+
+    public int getPagesRead() {
+        return -1;
+    }
+
+    public double getProgress() {
+        return -1;
+    }
+
+}
---
+++ src/main/model/Bookshelf.java
@@ -0,0 +1,20 @@
+package model;
+
+// Represent a bookshelf that stores all the books I currently need to read
+// that keep track of my total progress
+class Bookshelf {
+
+    public Bookshelf() {
+
+    }
+
+    public void totalProgressUpdate() {
+
+    }
+
+    public Book getBook() {
+        return null;
+    }
+
+
+}
